29a289fa79ce3950d18bdb7abdc5a2c772b7a28e
Matthias Stumpp <mstumpp@gmail.com>
2017-07-02 23:09:16 +0200

    Changed: filter arrays

--- swagger_api/api/controllers/db/db_controller.js
+++ swagger_api/api/controllers/db/db_controller.js
@@ -3,12 +3,15 @@ var web3 = require("../blockchain/connector.js");
 var DB = require('nosql');
 var eqcontracts = DB.load('./nosql_db/eqcontracts.nosql');
 
-
-
-
-
-
-
+function filterObjects(array) {
+    var flags = [], output = [], l = array.length, i;
+    for( i=0; i<l; i++) {
+        if( flags[array[i].eqcontract]) continue;
+        flags[array[i].eqcontract] = true;
+        output.push(array[i]);
+    }
+    return output;
+}
 
 module.exports = {
 
@@ -19,13 +22,14 @@ module.exports = {
     saveEQcontract: function(_eqcontract, _insurer, _customer, _status, _duration, _strength, _value) {
         eqcontracts.insert({ eqcontract: _eqcontract, insurer: _insurer, customer: _customer, status: _status, duration: _duration, strength: _strength, value: _value })
     },
+
     getAllCustomerEQContracts: function(_address) {
         return new Promise(function(resolve, reject) {
-            eqcontracts.one().make(function(builder) {
+            eqcontracts.find().make(function(builder) {
                 builder.where('customer', _address);
                 builder.callback(function(err, response) {
                     if (!err) {
-                        resolve(response);
+                        resolve(filterObjects(response));
                     } else {
                         reject(err);
                     }
@@ -33,13 +37,14 @@ module.exports = {
             });
         });
     },
+
     getAllInsurerEQContracts: function(_address) {
         return new Promise(function(resolve, reject) {
-            eqcontracts.one().make(function(builder) {
+            eqcontracts.find().make(function(builder) {
                 builder.where('insurer', _address);
                 builder.callback(function(err, response) {
                     if (!err) {
-                        resolve(response);
+                        resolve(filterObjects(response));
                     } else {
                         reject(err);
                     }
