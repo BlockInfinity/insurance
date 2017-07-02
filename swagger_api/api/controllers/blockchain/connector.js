44bfa89d3dfc8ff3a6fcdecaa20102d45edc9b7a
mgsgde <mgsgde@mgsgde@gmail.com>
2017-07-02 12:38:23 +0200

    added insurer controllers

--- swagger_api/api/controllers/blockchain/connector.js
+++ swagger_api/api/controllers/blockchain/connector.js
@@ -24,53 +24,20 @@ try {
     var jsonPath = path.join(__dirname, '..', '..', '..', '..', '/contracts/build/contracts/InsuranceContractFactory.json');
     var abi_eqfactory = fs.readFileSync(jsonPath).toString();
     abi_eqfactory = JSON.parse(abi_eqfactory).abi;
-    // console.log(abi_eqfactory);
-    // var abi_browser = [{ "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "contracts", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "_id", "type": "uint256" }], "name": "getEarthQuakeContract", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "createEarthQuakeContract", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "fuck", "type": "string" }], "name": "test", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_eqcontract", "type": "address" }, { "indexed": false, "name": "_id", "type": "uint256" }], "name": "EQContractCreation", "type": "event" }];
-
-
-    // if (String(abi_eqfactory) == String(abi_browser)) {
-    //     console.log("equal")
-    // } else {
-    //     console.log(abi_eqfactory);
-    //     console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-    //     console.log(abi_browser);
-
-    // }
-
-    // var contract_eqfactory = web3.eth.contract(abi_eqfactory);
-    // var contract_eqfactory_instance = contract_eqfactory.at(addresses.InsuranceContractFactory);
-    // web3.factory = contract_eqfactory_instance;
-    // console.log(web3.eth.accounts)
-    // console.log(web3.factory.address,123123)
-
-    // eth.getBalance(eth.accounts[0])
-    // console.log(eth);
-    // console.log(web3.fromWei(eth.getBalance(eth.accounts[0])));
-
-    // #############################
-    var browser_insurancecontractfactory_sol_insurancecontractfactoryContract = web3.eth.contract(abi_eqfactory)
-    // var browser_insurancecontractfactory_sol_insurancecontractfactory = browser_insurancecontractfactory_sol_insurancecontractfactoryContract.at("0xe6847c7729be0fe24d34c57718c9ce6948e7cc4e")
-    var browser_insurancecontractfactory_sol_insurancecontractfactory = browser_insurancecontractfactory_sol_insurancecontractfactoryContract.at(addresses.InsuranceContractFactory)
-
-    web3.factory =  browser_insurancecontractfactory_sol_insurancecontractfactory;
-
-    var x =  web3.factory.createEarthQuakeContract({ from: eth.accounts[0], gas: 8000000 })
-    var y =  web3.factory.getEarthQuakeContract(0);
-    console.log(x, 1)
-    console.log(y, 2);
 
 
     // #############################
+    var eqcontract_contract = web3.eth.contract(abi_eqfactory)
+    // var factory = eqcontract_contract.at("0xe6847c7729be0fe24d34c57718c9ce6948e7cc4e")
+    var factory = eqcontract_contract.at(addresses.InsuranceContractFactory)
 
+    web3.factory =  factory;
 
-    // var browser_insurancecontractfactory_sol_insurancecontractfactoryContract = web3.eth.contract([{ "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "contracts", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "_id", "type": "uint256" }], "name": "getEarthQuakeContract", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "createEarthQuakeContract", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "fuck", "type": "string" }], "name": "test", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_eqcontract", "type": "address" }, { "indexed": false, "name": "_id", "type": "uint256" }], "name": "EQContractCreation", "type": "event" }]);
-    // var browser_insurancecontractfactory_sol_insurancecontractfactory = browser_insurancecontractfactory_sol_insurancecontractfactoryContract.at("0xe6847c7729be0fe24d34c57718c9ce6948e7cc4e")
-
-    // var x = browser_insurancecontractfactory_sol_insurancecontractfactory.createEarthQuakeContract({ from: eth.accounts[0], gas: 8000000 })
-    // var y = browser_insurancecontractfactory_sol_insurancecontractfactory.getEarthQuakeContract(0);
+    // var x =  web3.factory.createEarthQuakeContract({ from: eth.accounts[0], gas: 8000000 })
+    // var y =  web3.factory.getEarthQuakeContract(0);
     // console.log(x, 1)
     // console.log(y, 2);
-    // #############################
+
 
     jsonPatheqcontract = path.join(__dirname, '..', '..', '..', '..', '/contracts/build/contracts/EarthQuakeContract.json');
     var abi_eqcontract = fs.readFileSync(jsonPatheqcontract).toString();
@@ -81,24 +48,9 @@ try {
     var eqcontract = eqcontract_contract.at(addresses.EarthQuakeContract)
     web3.eqcontract =  eqcontract;
 
-    web3.eqcontract.request(1,"1232",1,1,{from: eth.accounts[0], gas: 8000000});
+    // web3.eqcontract.request(1,"1232",1,1,{from: eth.accounts[1], gas: 8000000});
 
 
-
-    // var x =  web3.factory.createEarthQuakeContract({ from: eth.accounts[0], gas: 8000000 })
-    // var y =  web3.factory.getEarthQuakeContract(0);
-    // console.log(x, 1)
-    // console.log(y, 2);
-
-
-    // jsonPath = path.join(__dirname, '..','..','..','..','/contracts/build/contracts/EarthQuakeContract.json');
-    // var abi_KwhToken = fs.readFileSync(jsonPath).toString();
-    // abi_KwhToken = JSON.parse(abi_KwhToken).abi;
-    // var contract_KwhToken = web3.eth.contract(abi_KwhToken);
-    // var contract_KwhToken_instance = contract_KwhToken.at(addresses.EarthQuakeContract);
-    // web3.eqcontract = contract_KwhToken_instance;
-    // console.log(web3.eqcontract.address,1234124);
-    // console.log(web3.eqcontract.request(1,"asdad",1,1,{from: web3.eth.accounts[0]}));
 
 } catch (err) {
     throw new Error(err);
--- swagger_api/api/controllers/creation_controller.js
+++ swagger_api/api/controllers/creation_controller.js
@@ -5,27 +5,31 @@ var eth = web3.eth;
 module.exports = {
 
 
-  // create: function(req, res) {
-  //       try {
-  //           var address = req.swagger.params.createRequest.value.address;
+    create: function(req, res) {
+        try {
+            var address = req.swagger.params.createRequest.value.address;
 
-  //           if (address == "string") {
-  //               address = eth.accounts[0];
-  //           }
-  //           console.log(address);
-  //           console.log(web3.factory.createEarthQuakeContract);
-  //           console.log(web3.factory.address);
+            if (address == "string") {
+                address = eth.accounts[0];
+            }
+            console.log(address);
+            console.log(web3.factory.createEarthQuakeContract);
+            console.log(web3.factory.address);
 
-  //           web3.factory.createEarthQuakeContract({from:eth.accounts[0], gas: 8000000});
-            
-  //           // web3.factory.createEarthQuakeContract({from:address, gas: 8000000});
+            web3.factory.createEarthQuakeContract({ from: eth.accounts[0], gas: 8000000 });
 
-  //           res.statusCode = 200;
-  //           res.end(JSON.stringify({ "succeeded": true }));
-  //       } catch (error) {
-  //           res.statusCode = 500;
-  //           res.end(error.message);
-  //       }
-  //   }
+            web3.factory.EQContractCreation(function(error, result) {
+                var contractAddress = result.args._eqcontract;
+                var id = result.args._id;
+                res.statusCode = 200;
+                res.end(JSON.stringify({ "address": contractAddress, "id": id }));
+            })
+
+
+        } catch (error) {
+            res.statusCode = 500;
+            res.end(error.message);
+        }
+    }
 
 }
